e63fad112540f6c1c389daef46b1ed522beda703
justzerock <liu.zerock@gmail.com>
2022-10-16 01:43:12 +0800

    修改

--- source/js/scroll.js
+++ source/js/scroll.js
@@ -1,21 +1,53 @@
+let isScrollUp = false
+let isScrollDown = false
+
 const scrollToTop = () => {
   const scrollTop = document.documentElement.scrollTop || document.body.scrollTop
   if (scrollTop > 0) {
     window.requestAnimationFrame(scrollToTop)
-    window.scrollTo(0, scrollTop - scrollTop / 8)
+    window.scrollTo(0, scrollTop - scrollTop / 4)
+    isScrollUp = true
+  } else {
+    isScrollUp = false
   }
 }
-let btnGoTop = document.getElementsByClassName('go-top')[0]
-btnGoTop.onclick = function() {
-  scrollToTop()
+
+const scrollToBottom = () => {
+  (function smoothscroll() {
+    const currentScroll = document.documentElement.scrollTop || document.body.scrollTop; // 已经被卷掉的高度
+    const clientHeight = document.documentElement.clientHeight; // 浏览器高度
+    const scrollHeight = document.documentElement.scrollHeight; // 总高度
+    if (scrollHeight - 10 > currentScroll + clientHeight) {
+      window.requestAnimationFrame(smoothscroll);
+      window.scrollTo(0, currentScroll + (scrollHeight - currentScroll - clientHeight) / 4);
+      isScrollDown = true
+    } else {
+      isScrollDown = false
+    }
+  })();
+};
+
+
+let btnGoTop = document.getElementsByClassName('gotop')[0]
+btnGoTop.onclick = function () {
+  let btnClass = btnGoTop.getAttribute('class')
+  if (btnClass.indexOf('up') > -1 && !isScrollDown) {
+    scrollToTop()
+  } else if (!isScrollUp) {
+    scrollToBottom()
+  }
 }
 
 window.onscroll = function () {
   var g = document.documentElement.scrollTop || document.body.scrollTop;
-  if (g > 250) {
-    btnGoTop.style.transform = 'translate(0, 0)'
+  var h = document.documentElement.clientHeight
+  var s = document.documentElement.scrollHeight
+  if (g > (s-h)/2) {
+    btnGoTop.classList.add('up')
+    btnGoTop.ariaLabel = '滚动到顶部'
   }
-  else if (g < 250) {
-    btnGoTop.style.transform = 'translate(0, 3.5vw)'
+  else {
+    btnGoTop.classList.remove('up')
+    btnGoTop.ariaLabel = '滚动到底部'
   }
 }
